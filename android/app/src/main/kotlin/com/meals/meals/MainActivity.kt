f96287f25017ade251fb158982795adc8e87bafc
Mohammed Gaber Alabed <mohammed2017alabed@gmail.com>
2026-04-07 22:39:47 +0300

    Initial release: Meals app with Navigation and State Management

---
+++ android/app/src/main/kotlin/com/meals/meals/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.meals.meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,26 @@
+//import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:meals/models/meal.dart';
+
+
+
+
+class FavoriteMealNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealNotifier() : super([]);
+
+  bool toggleMealFavoriteState(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+    if (mealIsFavorite) {
+      state =state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state =[...state, meal];
+      return true;
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealNotifier, List<Meal>>((ref) {
+      return FavoriteMealNotifier();
+    });
